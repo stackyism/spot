27d3968782a530f0b835e9bb3d69364dc1fb2ca0
Saagar Takhi <stakhi08@gmail.com>
2015-07-11 12:50:17 +0530

    search enabled

--- app/js/controller.js
+++ app/js/controller.js
@@ -5,37 +5,12 @@ spot.controller('spotCtrl',['$scope', '$http', function($scope, $http){
 	$http.get('data/tables.json').success(function(response){
 		$scope.tables = response;
 	});
-/*spot.controller('spotCtrl',function($scope){
-	$scope.tables = [
-	{
-		'id': 1,
-		'name': 'Table 1',
-		'seat' : 4,
-	},
-	{
-		'id': 2,
-		'name': 'Table 2',
-		'seat' : 4,
-	},
-	{
-		'id': 3,
-		'name': 'Table 3',
-		'seat' : 4,
-	},
-	{
-		'id': 4,
-		'name': 'Table 4',
-		'seat' : 4,
-	},
-	{
-		'id': 5,
-		'name': 'Table 5',
-		'seat' : 4,
-	},
-	{
-		'id': 6,
-		'name': 'Table 6',
-		'seat' : 4,
-	}];
-});*/
 }]);
+spot.filter('custFilter', function(){
+	return function(text, input){
+		if(text==input){
+			return 'try';
+		}
+		else return 'no-change';
+	}
+});
